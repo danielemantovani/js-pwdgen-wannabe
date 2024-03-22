d0494e63b35c8a11038e37360a00fa9d8d9d2d48
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-22 15:31:06 +0100

    scaffolding: js initial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// TEST DI FUNZIONAMENTO
+// alert("ciao")
+
+// DOMANDA NOME
+let userName = prompt ("Caro utente immaginario, come ti chiami?"); //string
+console.log (userName, typeof userName);
+
+// DOMANDA COGNOME
+let userSurname = prompt ("e il tuo cognome?") //string
+console.log (userSurname, typeof userSurname);
+
+// DOMANDA COLORE PREFERITO
+let userColor = prompt ( "e invece il tuo colore preferito?")
+console.log (userColor, typeof userColor);
+
