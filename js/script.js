f415e3f8b5a8c7f3c1b3947e333909a65607e1e5
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-22 16:24:07 +0100

    feat: fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 // TEST DI FUNZIONAMENTO
 // alert("ciao")
 
+// DATI DI INPUT
+
 // DOMANDA NOME
-let userName = prompt ("Caro utente immaginario, come ti chiami?"); //string
+const userName = prompt ("Caro utente immaginario, come ti chiami?"); //string
 console.log (userName, typeof userName);
 
 // DOMANDA COGNOME
-let userSurname = prompt ("e il tuo cognome?") //string
+const userSurname = prompt ("e il tuo cognome?"); //string
 console.log (userSurname, typeof userSurname);
 
 // DOMANDA COLORE PREFERITO
-let userColor = prompt ( "e invece il tuo colore preferito?")
+const userColor = prompt ( "e invece il tuo colore preferito?"); //string
 console.log (userColor, typeof userColor);
+
+
+// ELABORAZIONE DEI DATI
+const userInfo = userName + userSurname + userColor; //string
+console.log (userInfo, typeof userInfo);
+
+
+// STAMPA DELL'OUTPUT
+const resultMessage = `${userInfo} 21`;
+console.log (resultMessage);
+
+
+// STAMPO IL MESSAGGIO IN PAGINA
+document.getElementById("result").innerHTML = resultMessage;
+
+
 
